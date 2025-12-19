0074f66fada07db8b39990d56b55b5f55745670a
Giovanni <grd5217@psu.edu>
2025-12-18 22:52:33 -0500

    Add login event listener and TESTING validation logic
    When user logs in with testing credentials (1,1), change screen to home page.  DELETE 1,1 LOGIN LATER
    
    If login fails, make err visible.  If login doesnt fail, make err hidden

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+
+const loginBut = document.getElementById('login-but');
+
+loginBut.addEventListener('click', function() {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value; 
+    const invalidLoginMsg = document.getElementById('invalid-login');
+
+
+    if (username === '1' && password === '1') {
+        alert('Login successful!');
+        window.location.href = 'user_home.html';
+        invalidLoginMsg.style.visibility = 'hidden';
+    }
+    else {
+        invalidLoginMsg.style.visibility = 'visible';
+    }
+});
